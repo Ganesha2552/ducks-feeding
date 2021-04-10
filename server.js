4c25ac076bab6dda8c05ca2dafc4c90310e438f7
Ganesha moorthy <ganesh.moorthy2552@gmail.com>
2021-04-10 17:19:38 +0530

    For delpoyment

--- server.js
+++ server.js
@@ -32,6 +32,8 @@ require("./config/passport")(passport);
 
 app.use("/api/users", users);
 app.use("/api/feeding", feeding);
-
-const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+const port = process.env.PORT || 5000; // process.env.port is Heroku's port 
 app.listen(port, () => console.log(`Server and running on port ${port} !`));
